6096737eb8385d757e95e290ac78cc2707a86206
asmae <asmaeaouassar333@example.com>
2024-11-30 14:58:32 +0100

    use thymeleaf

--- springboot-backend/src/main/java/net/javaguides/springboot/exception/ResourceNotFoundException.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/exception/ResourceNotFoundException.java
@@ -3,12 +3,22 @@ package net.javaguides.springboot.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(value = HttpStatus.NOT_FOUND) // Cette  indique que lorsque cette exception est levée, le serveur renverra une réponse HTTP avec le code d'état 404 (Not Found)
+/**
+ * Cette classe représente une exception personnalisée utilisée pour signaler qu'une ressource demandée n'a pas été trouvée
+ * Elle hérite de RuntimeException , permettant ainsi de gérer les erreurs liées aux ressources de manière centralisée
+ */
 public class ResourceNotFoundException extends RuntimeException{
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L; // identifier de manière unique la version de la classe lors de la sérialisation et de la désérialisation
 	
 	public ResourceNotFoundException(String message) {
 		super(message);
 	}
 }
+
+/**
+ * REMARQUE :
+ * Cette exception est typiquement utilisée dans ls contrôlurs REST pour gérer des scénarios où
+ * une ressource spécifique n'existe pas dans la BD
+ */
--- springboot-backend/src/main/java/net/javaguides/springboot/model/Employee.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/model/Employee.java
@@ -1,7 +1,11 @@
 package net.javaguides.springboot.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@Data
 @Entity
 @Table(name = "employees")
 public class Employee {
@@ -18,10 +22,7 @@ public class Employee {
 	
 	@Column(name = "email_id")
 	private String emailId;
-	
-	public Employee() {
-		
-	}
+
 	
 	public Employee(String firstName, String lastName, String emailId) {
 		super();
@@ -29,28 +30,5 @@ public class Employee {
 		this.lastName = lastName;
 		this.emailId = emailId;
 	}
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getEmailId() {
-		return emailId;
-	}
-	public void setEmailId(String emailId) {
-		this.emailId = emailId;
-	}
+
 }
